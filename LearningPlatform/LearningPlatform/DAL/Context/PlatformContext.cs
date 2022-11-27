8edd66731b6038dee4d57fafccd6a31ea8f58b18
Angelika Przeliorz <artgelika@gmail.com>
2022-11-27 23:36:17 +0100

    Add model of platform

---
+++ LearningPlatform/LearningPlatform/DAL/Context/PlatformContext.cs
@@ -0,0 +1,13 @@
+﻿using LearningPlatform.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearningPlatform.DAL.Context
+{
+    public class PlatformContext : DbContext
+    {
+        public PlatformContext(DbContextOptions<PlatformContext> opt) : base(opt)
+        { }
+
+        public DbSet<Platform> Platforms { get; set; }
+    }
+}
